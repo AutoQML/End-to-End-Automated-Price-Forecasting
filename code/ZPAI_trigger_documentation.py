7214cc9d37ea1cc64edf1bc18d3dea2d962aa1fa
Horst Stuehler <horst.stuehler@gmail.com>
2023-09-05 16:24:17 +0200

    Move documentation trigger into an own file.

---
+++ code/ZPAI_trigger_documentation.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+from datetime import date
+from ZPAI_common_functions import read_yaml
+
+from ZPAI_document_results_docx import document_results_docx
+
+###############################
+# Create dokumentation
+###############################
+# datasets = ['Caterpillar-308', 'Caterpillar-320', 'Caterpillar-323', 'Caterpillar-329', 'Caterpillar-330', 'Caterpillar-336', 'Caterpillar-950', 'Caterpillar-966', 'Caterpillar-D6', 'Caterpillar-M318']
+# datasets = ['merged-files']
+# datasets = ['Caterpillar-950', 'Caterpillar-966']
+datasets = ['Caterpillar-320']
+
+# define methods to be droped from the scatter plots
+# drop_methods = []
+# drop_methods = ['rf', 'svr-single', 'svr-integrated']
+
+
+##############################
+# Define data set version
+##############################
+# final | no-cr-full-set | 
+# dataset_version = 'no-cr-full-set'
+
+
+# Set date
+m_date = '2023-08-20'
+
+# create summery yaml file
+# File path within the summery directory for each measurement
+EXPLICIT_SUMMERY_FILE_PATH = Path('./measurements', 'summery', m_date)
+
+filename = "{}-{}.{}".format(m_date,'summery','yml')
+GLOBAL_YAML_SUMMERY_FILE = Path(EXPLICIT_SUMMERY_FILE_PATH, filename)
+
+######################################
+# LOAD CONFIGURATIONS
+######################################
+REPO_PATH = Path(__file__).parents[1]
+
+# Load configuration files
+general_conf = read_yaml(REPO_PATH / 'conf/general_config.yml')
+feature_conf = read_yaml(REPO_PATH / 'conf/feature_config.yml')
+autosklearn_conf = read_yaml(REPO_PATH / 'conf/auto_sklearn_config.yml')
+
+# Create global configuration file
+CFG = dict()
+CFG["general"] = general_conf
+CFG["features"] = feature_conf
+CFG["autosklearn"] = autosklearn_conf
+
+# CFG["general"]["start_date"] = get_current_date()
+CFG["general"]["start_date"] = m_date
+
+NUM_OF_MEASUREMENTS = 5
+
+# document_results_docx(datasets,m_date, GLOBAL_YAML_SUMMERY_FILE, EXPLICIT_SUMMERY_FILE_PATH)
+document_results_docx(datasets,
+                              NUM_OF_MEASUREMENTS = NUM_OF_MEASUREMENTS,
+                              GLOBAL_YAML_SUMMERY_FILE = GLOBAL_YAML_SUMMERY_FILE, 
+                              EXPLICIT_SUMMERY_FILE_PATH = EXPLICIT_SUMMERY_FILE_PATH, 
+                              config = CFG)
