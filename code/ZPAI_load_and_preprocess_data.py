01e66e011954b636ccf920aa02799b8a9de246b9
Horst Stuehler <horst.stuehler@gmail.com>
2023-10-12 18:13:42 +0200

    Add new functions.

--- code/ZPAI_load_and_preprocess_data.py
+++ code/ZPAI_load_and_preprocess_data.py
@@ -11,7 +11,34 @@ import matplotlib.pyplot as plt
 from ZPAI_prepare_data_for_ml import prepare_data_for_ml
 from ZPAI_common_functions import load_csv_data, create_path, read_yaml
 
-import re
+import featuretools as ft
+
+import shap
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from xgboost.sklearn import XGBRegressor
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+
+from sklearn.metrics import accuracy_score,confusion_matrix, mean_absolute_percentage_error
+
+from sklearn import tree
+
+# import xgboost as xgb
+
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+
+from featuretools.selection import (
+    remove_highly_correlated_features,
+    remove_highly_null_features,
+    remove_single_value_features,
+)
 
 # import featuretools as ft
 
@@ -135,6 +162,135 @@ def load_and_preprocess_data(datasets: list,
     # Replace NaN values with zeros
     merged_df[columns_to_replace] = merged_df[columns_to_replace].fillna(0)
 
-    # print(merged_df)
+    print(merged_df.info())
+
+
+    ###############################################
+    # SHAP part
+    # prepare the date and train it on XGBRegressor 
+    ###############################################
+
+    #get features
+    y = merged_df['price']
+    y = y.astype('category').cat.codes
+    X_cat = merged_df.drop('price', axis=1)
+
+    #fit encoder
+    enc = OneHotEncoder( categories=['object'])
+    enc.fit(X_cat)
+
+    #transform categorical features
+    X_encoded = enc.transform(X_cat).toarray()
+
+    #create feature matrix
+    feature_names = X_cat.columns
+    new_feature_names = enc.get_feature_names_out(feature_names)
+
+    X = pd.DataFrame(X_encoded, columns= new_feature_names)
+
+    print(f"Values for X:\n {X}")
+
+    model = XGBRegressor(n_estimators=1000, max_depth=10, learning_rate=0.001)
+    # Fit the Model
+    model.fit(X, y)
+
+    #Get predictions
+    y_pred = model.predict(X)
+
+    # print(f"Confusion matrix: {confusion_matrix(y, y_pred)}")
+    # print(f"Accuracy score: {accuracy_score(y, y_pred)}")
+
+    print(f"mean absolute percentage erro: {mean_absolute_percentage_error(y, y_pred)}")
+
+
+    #get number of unique categories for each feature 
+    n_categories = []
+    for feat in feature_names[:-1]:
+        n = X_cat[feat].nunique()
+        n_categories.append(n)
+        
+    print("Feature categories: ", n_categories)
+
+
+
+    # # Prepare X and Y 
+    # X = pd.get_dummies(merged_df)
+    # X.drop(['price'], inplace=True, axis=1)
+    # y = merged_df['price']
+
+    # model = XGBRegressor(n_estimators=1000, max_depth=10, learning_rate=0.001)
+    # # Fit the Model
+    # model.fit(X, y)
+
+    # load JS visualization code to notebook
+    # shap.initjs()
+    ##########################################
+    # Interpret this model using SHAP values. To do this, we pass our model 
+    # into the SHAP Explainer function. This creates an explainer object. 
+    # We use this to calculate SHAP values for every observation in the feature matrix.
+    #########################################
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(X)
+
+    # shap.plots.waterfall(shap_values[0])
+    plt.figure()
+    shap.plots._waterfall.waterfall_legacy(explainer.expected_value, shap_values[0])
+    plt.savefig('waterfall.png')
+
+    # print(f"Shap values: {shap_values}")
+
+    i = 4
+    # shap.force_plot(explainer.expected_value, shap_values[i], features=X.iloc[i], feature_names=X.columns)
+    # shap.summary_plot(explainer.expected_value, shap_values[i], features=X.iloc[i], feature_names=X.columns, show=False)
+    # shap.summary_plot(shap_values, features=X, feature_names=X.columns, show=False)
+    # plt.savefig('scratch.png')
+    plt.figure()
+    shap.summary_plot(shap_values, features=X, feature_names=X.columns, plot_type='bar', show=False)
+    plt.savefig('barplot.png')
+
+
+    # # Reset the index
+    # merged_df.reset_index(inplace=True)
+
+    # ###################################
+    # # use featuretools to create new or delete unneeded features
+    # ###################################
+    # # es = ft.EntitySet("taxi")
+
+    # es = ft.EntitySet(id="models_entity_set")
+
+    # # create an entity set
+    # es = es.add_dataframe(
+    #     dataframe_name="merged_df",
+    #     dataframe=merged_df,
+    #     index="index",
+    # )
+    # # print the entityset
+    # print(f"EntytySet: \n {es['merged_df']}")
+    # print(f"EntytySet: \n {es['merged_df']}")
+    
+
+    # feature_matrix, features = ft.dfs(entityset=es,
+    #                            max_features  = 6,
+    #                            target_dataframe_name="merged_df")
+    
+    # # features = ft.dfs(entityset=es,
+    # #             target_dataframe_name="merged_df",
+    # #             features_only=True)
+    
+
+    # print(f"Feature matrix: \n {feature_matrix}")
+    # print(f"Features: \n {features}")
+
+    # ft.selection.remove_highly_null_features(feature_matrix)
+
+    # new_fm, new_features = remove_single_value_features(feature_matrix, features=features)
+    # # print(f"NEW Feature matrix: \n {new_fm}")
+
+    # # feature_matrix_enc, features_enc = ft.encode_features(feature_matrix, features)
+    # # print(feature_matrix_enc)
+
+    # merged_df[columns_to_replace] = merged_df[columns_to_replace].fillna(0)
+
 
     return(merged_df)
